53ee5e58d6d5cb4c5110a8d70a1d6806222eb60b
Ben Nelson <nelson.ben.c@gmail.com>
2013-02-17 19:03:57 -0700

    adding in more tests for filtering

--- stream.rb
+++ stream.rb
@@ -8,6 +8,21 @@ TweetStream.configure do |config|
     config.auth_method = :oauth
 end
 
-TweetStream::Client.new.sample do |status|
-    puts "#{status.user.screen_name}: #{status.text}"
-end
+	puts "Beginning stream..."	
+	begin
+		#approximating the lat and long of the lower 48 US -125,20,-125,50,-70,25,-70,50
+#	TweetStream::Client.new.sample do |status, client|
+	TweetStream::Client.new.locations('-74,40,-73,41') do |status, client|
+		data = {"created_at" => Time.parse(status.created_at), "text" => status.text, "geo" => status.geo, "coordinates" => status.coordinates, "id" => status.id, "id_str" => status.id_str}
+		puts data.to_s
+	end
+	rescue Interrupt
+			puts "Closing connection..."
+	end
+#	TweetStream::Daemon.new.on_error do |message|
+#		puts "An error occurred..."
+#	end.filter({"locations" => "-125.00, 20.00, -125.00, 50.00, -70.00, 25.00, -70.00, 50.00"}) do |status|
+	  # Do things when nothing's wrong
+#	  puts data.to_s
+#	end
+
